35d6a26fdae5d30067a3d169e02cfcda687ec7b8
ashish <shrotiashish759@gmail.com>
2020-08-28 20:46:17 +0530

    first commit

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -10,8 +10,7 @@ function TodoInput(props) {
           <div class="card">
               <img class="card-img-top" src="holder.js/100x180/" alt=""/>
               <div class="card-body">
-                  <h4 class="card-title">Title</h4>
-                  <p class="card-text">Text</p>
+                 
                   <div class="form-group">
                     <label for=""></label>
                     <form onSubmit = {handleSubmit}>
@@ -32,7 +31,7 @@ function TodoInput(props) {
                    </form>
                     
                    
-                    <small id="helpId" class="form-text text-muted">Help text</small>
+                  
                   </div>
               </div>
           </div>
